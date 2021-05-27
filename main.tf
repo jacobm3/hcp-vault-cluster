61d9eeb5de68d8bf437d9511d44c3b412b496f1b
Jacob <jacobm3@gmail.com>
2021-05-27 06:17:55 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "hcp_vault_cluster" "demo_vault_cluster" {
+  hvn_id     = hcp_hvn.main.hvn_id
+  cluster_id = "hcp-tf-demo-vault-cluster"
+  public_endpoint = true
+}
+
+resource "hcp_vault_cluster_admin_token" "demo_vault_admin_token" {
+  cluster_id = hcp_vault_cluster.demo_vault_cluster.cluster_id
+}
+
+resource "hcp_hvn" "main" {
+  hvn_id         = "main-hvn"
+  cloud_provider = "aws"
+  region         = "us-west-2"
+  cidr_block     = "10.20.1.0/24"
+}
