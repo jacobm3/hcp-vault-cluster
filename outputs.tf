e4bb7feffcd51976227400650d304fefc37bff94
Jacob <jacobm3@gmail.com>
2021-05-27 06:35:47 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vault_version" {
+  value = hcp_vault_cluster.demo_vault_cluster.vault_version
+}
+
+output "organization_id" {
+  value = hcp_vault_cluster.demo_vault_cluster.organization_id
+}
+
+output "region" {
+  value = hcp_vault_cluster.demo_vault_cluster.region
+}
+
+output "vault_private_endpoint_url" {
+  value = hcp_vault_cluster.demo_vault_cluster.vault_private_endpoint_url
+}
+
+output "vault_public_endpoint_url" {
+  value = hcp_vault_cluster.demo_vault_cluster.vault_public_endpoint_url
+}
+
+output "admin_token" {
+  value = hcp_vault_cluster_admin_token.demo_vault_admin_token.token
+}
+
