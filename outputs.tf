34bd6b61252ee78be7529785d3e0f1d791127c66
Jacob <jacobm3@gmail.com>
2021-05-27 07:08:57 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -18,8 +18,3 @@ output "vault_public_endpoint_url" {
   value = hcp_vault_cluster.demo_vault_cluster.vault_public_endpoint_url
 }
 
-output "admin_token" {
-  value = hcp_vault_cluster_admin_token.demo_vault_admin_token.token
-  sensitive = true
-}
-
